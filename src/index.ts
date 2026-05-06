eb49577ad2322b9fba0cf1696d9e41ea2873cb95
daniel-ayivor <danielkofiayivor23@gmail.com>
2026-05-06 12:53:06 +0000

    UI changes

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,16 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
+
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173",
+      "http://localhost:8080",
+      "http://localhost:8081",
+    "http://localhost:3000",
+    "http://localhost:5173"
+  ],
   credentials: true
 }));
 app.use(express.json());
